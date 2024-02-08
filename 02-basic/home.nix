c387381632fb640f3ba4a581a962840dc1a87ef3
Brandon Fulljames <bfullj@gmail.com>
2024-02-09 00:01:45 +0900

    Fix vars

--- 02-basic/home.nix
+++ 02-basic/home.nix
@@ -1,21 +1,23 @@
 { lib, pkgs, ... }: {
-  # Install packages from https://search.nixos.org/packages
-  home.packages = with pkgs; [
-    # Says hello.  So helpful.
-    hello
+  home = {
+    # Install packages from https://search.nixos.org/packages
+    packages = with pkgs; [
+      # Says hello.  So helpful.
+      hello
 
-    # Another package, just to show what a list looks like.
-    # Notice there's no commas anywhere.
-    cowsay
-  ];
+      # Another package, just to show what a list looks like.
+      # Notice there's no commas anywhere.
+      cowsay
+    ];
 
-  # This needs to be set to your actual username.
-  username = "myusername";
-  homeDirectory = "/home/myusername";
+    # This needs to be set to your actual username.
+    username = "myusername";
+    homeDirectory = "/home/myusername";
 
-  # Don't ever change this after the first build.
-  # It tells home-manager what the original state schema
-  # was, so it knows how to go to the next state.  It
-  # should NOT update when you update your system!
-  home.stateVersion = "23.11";
+    # Don't ever change this after the first build.
+    # It tells home-manager what the original state schema
+    # was, so it knows how to go to the next state.  It
+    # should NOT update when you update your system!
+    stateVersion = "23.11";
+  };
 }
